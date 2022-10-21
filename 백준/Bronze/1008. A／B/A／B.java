f468558183d8ca99c9109a839b8c61e002145a90
Nicole <florachoi1124@naver.com>
2022-10-21 09:43:39 +0900

    [Bronze V] Title: A/B, Time: 120 ms, Memory: 15996 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    double A, B;
+    A = sc.nextDouble();
+    B = sc.nextDouble();
+    sc.close();
+    System.out.println(A/B); 
+        
+    }                        
+}
