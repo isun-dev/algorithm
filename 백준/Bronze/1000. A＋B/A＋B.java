0e6232176cac9ea89e23817e0e06e19904a9811f
Nicole <florachoi1124@naver.com>
2022-10-21 10:20:38 +0900

    Update and rename A＋B.py to A＋B.java

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt()+sc.nextInt());     
+        sc.close();
+    }
+}
