36c22956d8610ac41315b713c5c7093e60de98cb
Nicole <florachoi1124@naver.com>
2022-10-21 09:50:03 +0900

    [Bronze V] Title: 새싹, Time: 72 ms, Memory: 11204 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String s = "         ,r'\"7\n"
+                + "r`-_   ,'  ,/\n"
+                + " \\. \". L_r'\n"
+                + "   `~\\/\n"
+                + "      |\n"
+                + "      |\n";
+
+        System.out.println(s);
+    }
+}
