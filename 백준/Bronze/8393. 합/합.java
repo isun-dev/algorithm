3d28d5c9fee942bc4454adda3861ff0fc0b83c05
Nicole <florachoi1124@naver.com>
2022-10-21 09:54:20 +0900

    [Bronze V] Title: 합, Time: 108 ms, Memory: 12824 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for(int i = 1 ; i <= n; i++){
+            sum += i;
+        }System.out.println(sum);
+    }
+}
