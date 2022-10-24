14a921048b14efd057fbba8a3f48f8eec9069d38
Nicole <florachoi1124@naver.com>
2022-10-24 19:27:54 +0900

    [Bronze II] Title: OX퀴즈, Time: 136 ms, Memory: 13504 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 반복된 만큼 카운트를 줘서 풀기
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        String[] arr = new String[count];
+
+        for (int i = 0; i < count; i++) {
+            int sum = 0;
+            int repeatCount = 0;
+            arr[i] = sc.next();
+            char[] charArray = arr[i].toCharArray();
+            for (int j = 0; j < charArray.length; j++) {
+                if (charArray[j] == 'X') {
+                    sum += 0;
+                    repeatCount = 0;
+                } else if (charArray[j] == 'O') {
+                    repeatCount++;
+                    sum += repeatCount;
+                }
+            }
+            System.out.println(sum);
+        }
+
+    }
+}
