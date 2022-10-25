286be2a4c39112efe5d0f41dace2b23bf1b5dae8
Nicole <florachoi1124@naver.com>
2022-10-25 17:04:18 +0900

    [Bronze V] Title: 아스키 코드, Time: 72 ms, Memory: 11436 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // System.in.read()는 기본적으로 아스키코드로 값을 받는다.
+        System.out.println(System.in.read());
+    }
+}
