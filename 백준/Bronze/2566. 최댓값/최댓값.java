906eb843324e94af876999b21d634754c0f7e523
Nicole <florachoi1124@naver.com>
2022-10-25 09:36:18 +0900

    Update 최댓값.java

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,34 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int[][] arr = new int[9][9];
-
-		for (int i = 0; i < 9; i++) {
-			String[] strArr = br.readLine().split(" ");
-			for (int j = 0; j < strArr.length; j++) {
-				arr[i][j] = Integer.parseInt(strArr[j]);
-			}
-		}
-
-		int num = 0;
-		int rowOrder = 0;
-		int columnOrder = 0;
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				if (num < arr[i][j]) {
-					num = arr[i][j];
-					rowOrder = i;
-					columnOrder = j;
-				}
-			}
-		}
-
-		System.out.println(num);
-		System.out.println((rowOrder + 1) + " " + (columnOrder + 1));
-	}
+        int[][] arr = new int[9][9];
+        for (int i = 0; i < arr.length; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        int max = 0;
+        int col = 0;
+        int row = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                    col = i;
+                    row = j;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.println((col + 1) + " " + (row + 1));
+    }
 }
