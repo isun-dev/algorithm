8aeee90c3b59da34854179dcfaea0ac02f77509f
Nicole <florachoi1124@naver.com>
2022-10-25 12:52:01 +0900

    [Bronze I] Title: 색종이, Time: 120 ms, Memory: 13088 KB -BaekjoonHub

---
+++ 백준/Bronze/2563. 색종이/색종이.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        int result = 0;
+        int[][] arr = new int[100][100];
+
+        for (int i = 0; i < count; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    arr[j][k] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (arr[i][j] == 1) {
+                    result++;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
