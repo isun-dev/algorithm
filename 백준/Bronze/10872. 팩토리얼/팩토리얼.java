b1e2d8960d4967e051bb8381a026dccb5b64822f
Nicole <florachoi1124@naver.com>
2022-11-01 08:24:22 +0900

    [Bronze V] Title: 팩토리얼, Time: 108 ms, Memory: 12832 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = factorial(n);
+
+        System.out.println(result);
+    }
+
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
