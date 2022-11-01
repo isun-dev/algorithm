d2ba15417a5892ab1c66856c8b8e641b93673e0e
Nicole <florachoi1124@naver.com>
2022-11-02 08:51:44 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 108 ms, Memory: 12836 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = fibonacci(n);
+
+        System.out.println(result);
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
