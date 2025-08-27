ad60d84bb77de7bc6bd236c516d69c5b71dcd878
Nicole <florachoi1124@naver.com>
2025-08-27 15:35:13 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.51 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] num_list) {
+        Arrays.sort(num_list);
+        return Arrays.copyOfRange(num_list, 5, num_list.length);
+    }
+}
